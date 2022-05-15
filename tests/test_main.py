95026d7ad37ba6ef2d34862699492e8be1584fc7
Mikhail Zakharov <mzakharo@gmail.com>
2022-05-15 13:45:10 -0400

    add test assertions"

--- tests/test_main.py
+++ tests/test_main.py
@@ -13,11 +13,14 @@ def test1():
     ts = datetime.datetime.now().replace(microsecond=0)
     res = upload_result(ts, FILENAME, SAVEDIR, res, min_confidence=0, dry=DRY, force_telegram=True)
     send_telegram_delayed(delayed_telegrams, ts, res, 1, dry=DRY)
+    assert len(delayed_telegrams) == 1
 
     #update but not send
     ts += datetime.timedelta(seconds=1)
     send_telegram_delayed(delayed_telegrams, ts, res, 1, dry=DRY)
+    assert len(delayed_telegrams) == 1
 
     # send message
     ts += datetime.timedelta(seconds=1)
     send_telegram_delayed(delayed_telegrams, ts, None, 1, dry=DRY)
+    assert len(delayed_telegrams) == 0
